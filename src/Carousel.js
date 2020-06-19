3c22e9fbb9e39408bd3790bb3470ef3c50bc5ec1
Anastasia <slyadneva.anastasia@gmail.com>
2020-06-19 13:06:33 -0500

    Add carousel  and details routes

---
+++ src/Carousel.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+
+class Carousel extends Component {
+  state = {
+    photos: [],
+    active: 0,
+  };
+
+  // must be static
+  // takes in a new set of props and returns a new set of state
+  static getDerivedStateFromProps({ media }) {
+    let photos = ["http://placecorgi.com/600/600"];
+
+    if (media.length) {
+      photos = media.map(({ large }) => large);
+    }
+
+    // return an obj that we want to be merged into state
+    return { photos: photos };
+  }
+
+  handleIndexClick = ({ target }) => {
+    this.setState({ active: +target.dataset.index });
+  };
+
+  render() {
+    const { photos, active } = this.state;
+    return (
+      <div className="carousel">
+        <img src={photos[active]} alt="animal" />
+        <div className="carousel-smaller">
+          {photos.map((photo, idx) => (
+            <img
+              key={photo}
+              onClick={this.handleIndexClick}
+              data-index={idx}
+              src={photo}
+              className={idx === active && "active"}
+              alt="animal thumbnail"
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
--- src/Details.js
+++ src/Details.js
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { Component } from "react";
+import pet from "@frontendmasters/pet";
+import Carousel from "./Carousel";
 
-const Details = (props) => {
-  return <div>Details</div>;
-};
+class Details extends Component {
+  state = {
+    loading: true,
+  };
+
+  componentDidMount() {
+    const { id } = this.props;
+    pet
+      .animal(id)
+      .then(
+        ({ animal: { name, type, contact, description, photos, breeds } }) => {
+          this.setState({
+            name: name,
+            animal: type,
+            location: `${contact.address.city}, ${contact.address.state}`,
+            description: description,
+            media: photos,
+            breed: breeds.primary,
+            loading: false,
+          });
+        },
+        console.error
+      );
+  }
+
+  render() {
+    const { loading } = this.state;
+    if (loading) {
+      return <h1>Loading...</h1>;
+    }
+
+    const { name, animal, location, description, media, breed } = this.state;
+
+    return (
+      <div className="details">
+        <Carousel media={media} />
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Details;
